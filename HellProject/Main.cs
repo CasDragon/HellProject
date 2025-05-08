625c56ee72009cc1be42e322c9e756e6c557a13d
CasDragon <redyugi112@gmail.com>
2025-05-08 14:44:07 -0400

    Add project files.

---
+++ HellProject/Main.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using BlueprintCore.Utils;
+using HarmonyLib;
+using Kingmaker.Blueprints.JsonSystem;
+using UnityModManagerNet;
+
+namespace HellProject
+{
+    public static class Main
+    {
+        internal static Harmony HarmonyInstance;
+        internal static UnityModManager.ModEntry.ModLogger log;
+
+        public static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            log = modEntry.Logger;
+            modEntry.OnGUI = OnGUI;
+            HarmonyInstance = new Harmony(modEntry.Info.Id);
+            HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+            return true;
+        }
+
+        public static void OnGUI(UnityModManager.ModEntry modEntry)
+        {
+
+        }
+
+        [HarmonyPatch(typeof(BlueprintsCache))]
+        public static class BlueprintsCaches_Patch
+        {
+            private static bool Initialized = false;
+
+            [HarmonyPriority(Priority.First)]
+            [HarmonyPatch(nameof(BlueprintsCache.Init)), HarmonyPostfix]
+            public static void Init_Postfix()
+            {
+                try
+                {
+                    if (Initialized)
+                    {
+                        log.Log("Already initialized blueprints cache.");
+                        return;
+                    }
+                    Initialized = true;
+
+                    log.Log("Patching blueprints.");
+                    StuffAndThings.Configure();
+                }
+                catch (Exception e)
+                {
+                    log.Log(string.Concat("Failed to initialize.", e));
+                }
+            }
+        }
+    }
+}
---
+++ HellProject/StuffAndThings.cs
@@ -0,0 +1,45 @@
+﻿using BlueprintCore.Blueprints.CustomConfigurators.Classes;
+using BlueprintCore.Blueprints.References;
+using BlueprintCore.Utils;
+using BlueprintCore.Utils.Types;
+using Kingmaker.AreaLogic.Cutscenes;
+using Kingmaker.Blueprints.Classes;
+using Kingmaker.Designers.EventConditionActionSystem.Actions;
+using Kingmaker.Designers.Mechanics.Facts;
+using Kingmaker.Utility;
+using System.Linq;
+
+namespace HellProject
+{
+    internal class StuffAndThings
+    {
+        public static void Configure()
+        {
+            ProgressionConfigurator devilprogression = ProgressionConfigurator.For(ProgressionRefs.DevilProgression);
+            Main.log.Log("Patching for Aeon gazes");
+            AeonGaze(devilprogression);
+            Main.log.Log("Patching for Hells Authority");
+            HellsAuthority(devilprogression);
+            devilprogression.Configure();
+        }
+        public static void AeonGaze(ProgressionConfigurator progression)
+        {
+            progression.RemoveComponents(c => c is RemoveFeatureOnApply);
+            LevelEntry[] entries = LevelEntryBuilder.New()
+                    .AddEntry(0, FeatureRefs.AeonGazeFeatureEndless.Reference.Get())
+                    .AddEntry(0, FeatureRefs.AeonGazeFeature.Reference.Get())
+                    .GetEntries();
+            progression.AddToLevelEntries(entries);
+            progression.AddRemoveFeatureOnApply(FeatureRefs.DragonAzataCompanionFeature.Reference.Get());
+            CommandAction aeontodevil = BlueprintTool.Get<CommandAction>("301e51edf1251a9489244d37421d89fb");
+            var test = aeontodevil.Action.Actions.ToList();
+            test.Remove(a => a is Conditional);
+            aeontodevil.Action.Actions = test.ToArray();
+        }
+        public static void HellsAuthority(ProgressionConfigurator progression)
+        {
+            progression.RemoveFromLevelEntries(3, FeatureRefs.HellsPowersFeature.Reference.Get());
+            progression.AddToLevelEntry(0, FeatureRefs.HellsPowersFeature.Reference.Get());
+        }
+    }
+}
